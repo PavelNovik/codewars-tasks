42447d5488b0dbacd3c94183a375c19722295ac7
Pavel Novik <brn2befree@gmail.com>
2023-10-26 16:39:19 +0300

    dop task from it-incubator

--- script.js
+++ script.js
@@ -84,3 +84,97 @@
 // console.log(isDivisible(12, 3, 4));
 // console.log(isDivisible(8, 3, 4));
 // console.log(isDivisible(48, 3, 4));
+
+//1. Реализуйте функцию, которая принимает параметром подсторку, число повторов и разделитель, а возвращает сторку, состоящую из указанного количества повторов подстроки с использованием разделителя.
+// repeatString("yo", 3, " ") => "yo yo yo"
+// repeatString("yo", 3, ",") => "yo,yo,yo"
+// for или str.repeat()
+
+const repeatString = (str, x, div) => {
+  //   return new Array(x).fill(str).join(div);
+  return (str + ' ').repeat(x).trim().replaceAll(' ', div);
+};
+
+// console.log(repeatString('xxx', 4, ' '));
+// console.log(repeatString('xxx', 4, ','));
+
+//2. Реализуйте функцию, которая принимает параметром сторку и подстроку, а возвращает true, если строка начинается с указанной подстроки, в противном случае - false. Регистр не учитывается.
+// checkStart("Incubator", "inc") => true
+// checkStart("Incubator", "yo") => false
+// str.startWith() slice indexOF
+
+const checkStart = (str, alf) => {
+  //   return str.toLowerCase().startsWith(alf.toLowerCase());
+  //   return str.slice(0, alf.length).toLowerCase() === alf.toLowerCase();
+  return str.toLowerCase().indexOf(alf.toLowerCase()) === 0 ? true : false;
+};
+
+// console.log(checkStart('Incubator', 'inc'));
+// console.log(checkStart('Incubator', 'yo'));
+
+//3. Реализуйте функцию, которая принимает параметром строку и число (количество символов), а возвращает строку из параметров, обрезанную до указанного количества символов и завершает её многоточием.
+//truncateString("Всем студентам инкубатора желаю удачи!", 10) => "Всем студе..."
+
+const truncateString = (str, x) => {
+  return str.slice(0, x) + '...';
+};
+
+// console.log(truncateString('Всем студентам инкубатора желаю удачи!', 10));
+
+//4. Реализуйте функцию, которая принимает параметром сторку (предложение) и возвращает самое короткое слово в предложении, если в параметрах пустая строка, то возвращает null.
+// getMinLengthWord("Всем студентам инкубатора желаю удачи!") => "Всем"
+// getMinLengthWord("") => null
+// split()
+
+const getMinLengthWord = (str) => {
+  return str ? str.split(' ').sort((a, b) => a.length - b.length)[0] : null;
+};
+
+// console.log(getMinLengthWord('Всем студентам инкубатора желаю удачи!'));
+// console.log(getMinLengthWord(''));
+
+//5. Реализуйте функцию, которая принимает параметром сторку (предложение) и возвращает то же предложение, где все слова написаны строчными, но начинаются с заглавных букв.
+// setUpperCase("всем стУдентам инкуБатора Желаю удачИ!") => "Всем Студентам Инкубатора Желаю Удачи!"
+
+// !!!!!!!!!!!!!!!!!!После решения 5 задач - поднимаем руку!!!!!!!!
+
+const setUpperCase = (str) => {
+  return (
+    str
+      .toLowerCase()
+      .split(' ')
+      // .map((w) => w[0].toUpperCase().concat(w.slice(1)))
+      .map((w) => w.replace(w[0], w[0].toUpperCase()))
+      .join(' ')
+  );
+};
+
+// console.log(setUpperCase('всем стУдентам инкуБатора Желаю удачИ!'));
+
+//6. Реализуйте функцию, котрая принимает параметрами строку и подстроку. Если все
+// символы подстроки содержаться в стороке - возвращает true, если нет -
+// возвращает false. Проверка проводится без учёта регистра и без учётом
+// повторяющихся символов.
+//* попробовать учитывать повтор символов в подстроке
+
+// isIncludes("Incubator", "Cut") => true
+// isIncludes("Incubator", "table") => false
+// isIncludes("Incubator", "inbba") => true
+// isIncludes("Incubator", "inba") => true
+// isIncludes("Incubator", "Incubatorrr")=> true
+
+const isIncludes = (str, sym) => {
+  return sym
+    .toLowerCase()
+    .split('')
+    .map((c) => str.toLowerCase().includes(c))
+    .includes(false)
+    ? false
+    : true;
+};
+
+console.log(isIncludes('Incubator', 'Cut'));
+console.log(isIncludes('Incubator', 'table'));
+console.log(isIncludes('Incubator', 'inbba'));
+console.log(isIncludes('Incubator', 'inba'));
+console.log(isIncludes('Incubator', 'Incubatorrr'));
